24454b9befa65ee91e1f89ee82a6681b14dc272c
Adam182Green <adam182green@gmail.com>
2017-04-27 14:56:29 +0200

    Add files via upload

--- java/com/gsb/parapharmacie/HomeActivity.java
+++ java/com/gsb/parapharmacie/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.gsb.parapharmacie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -17,7 +20,28 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        ((TextView) findViewById(R.id.texte)).setText("Menu");
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuActionCommande:
+                Toast.makeText(getApplicationContext(), "Commande", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menuActionPharmacie:
+                Toast.makeText(getApplicationContext(), "Pharmacie", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menuItemProfil:
+                Toast.makeText(getApplicationContext(), "Mon Profil", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menuItemCommandes:
+                Toast.makeText(getApplicationContext(), "Mes Commandes", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

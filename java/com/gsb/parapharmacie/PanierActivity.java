23dd0904c01a69e31ae30182ce8dc1907a52c94d
Adam182Green <adam182green@gmail.com>
2017-05-01 11:01:10 +0200

    Add files via upload

--- java/com/gsb/parapharmacie/PanierActivity.java
+++ java/com/gsb/parapharmacie/PanierActivity.java
@@ -1,13 +1,24 @@
 package com.gsb.parapharmacie;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.gsb.parapharmacie.Adapters.PanierAdapter;
+import com.gsb.parapharmacie.Adapters.PanierModifiableAdapter;
 import com.gsb.parapharmacie.Application.Parapharmacie;
+import com.gsb.parapharmacie.Models.Produit;
 import com.gsb.parapharmacie.Models.ProduitCommandeClient;
+import com.gsb.parapharmacie.Technical.Dialog;
 import com.gsb.parapharmacie.Technical.Utility;
 
 import java.util.List;
@@ -15,7 +26,7 @@ import java.util.List;
 public class PanierActivity extends AppCompatActivity {
 
     ListView panierLV;
-    TextView prixTotalTV;
+    public TextView prixTotalTV;
     List<ProduitCommandeClient> panier;
 
     @Override
@@ -27,6 +38,23 @@ public class PanierActivity extends AppCompatActivity {
 
         panier = ((Parapharmacie) getApplication()).getPanier();
 
+        setPrixTotal();
+
+        PanierModifiableAdapter panierAdapter = new PanierModifiableAdapter(PanierActivity.this, panier, prixTotalTV);
+        panierLV.setAdapter(panierAdapter);
+
+        panierLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Produit produit = (Produit) panierLV.getItemAtPosition(position);
+                Intent intent = new Intent(PanierActivity.this, ProduitActivity.class);
+                intent.putExtra("produit", produit);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setPrixTotal() {
         if (panier.size() == 0) {
             prixTotalTV.setText("Votre panier est vide.");
         } else {
@@ -34,12 +62,8 @@ public class PanierActivity extends AppCompatActivity {
             for (ProduitCommandeClient pCC : panier) {
                 prixTotal += pCC.getProduit().getPrix() * pCC.getQuantite();
             }
-            prixTotalTV.setText("Prix total :" + Utility.roundPrice(prixTotal));
+            prixTotalTV.setText("Prix total :" + Utility.roundPrice(prixTotal) + "€");
         }
-
-
-        PanierAdapter panierAdapter = new PanierAdapter(getApplicationContext(), panier);
-        panierLV.setAdapter(panierAdapter);
     }
 
     private void setViews() {
